bcd2fc4ef5755a102e15917f13b8768b81cab08e
nick culpin <njculpin@gmail.com>
2021-04-24 16:52:41 -0700

    Update player.js

--- components/player.js
+++ components/player.js
@@ -38,13 +38,16 @@ export default function Player(props){
   }
 
   const timeToSeconds = (str) => {
-    var p = str.split(':'),
-        s = 0, m = 1;
-    while (p.length > 0) {
-        s += m * parseInt(p.pop(), 10);
-        m *= 60;
+    if (str){
+      var p = str.split(':'),
+      s = 0, m = 1;
+      while (p.length > 0) {
+          s += m * parseInt(p.pop(), 10);
+          m *= 60;
+      }
+      return s;
     }
-    return s;
+    return 0
   }
 
   const handlePlay = () => {
